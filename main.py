376a709c75dfb81d46e89a90c1d1fab4f64cf5cd
UltraHispeed-Elite <62296749+UltraHispeed-Elite@users.noreply.github.com>
2024-09-15 21:43:36 -0500

    sunday evening work
    first three steps of the conversion program is created:
    - creation of the "resource_packs", "behavior_packs", and "outputs" folders
    - detection of all resource and behavior packs that need to be converted
    - creation of "asset" and "data" folders for outputted projects

---
+++ main.py
@@ -0,0 +1,51 @@
+import os 
+
+#template code
+
+#phase one, creates the directories if they don't already exist
+def makemydir(whatever):
+  try:
+    os.makedirs(whatever)
+  except OSError:
+    print(whatever+" already exists")
+    pass
+
+#phase two, lists all behavior and resource packs that need to be converted
+def list_items(dir):
+    # Get the list of all files and directories
+    path = dir
+    dir_list = os.listdir(path)
+    print("Files and directories in '", path, "' :")
+    # prints all files
+    print(dir_list)
+    return dir_list
+
+#phase three, creates new directories based on the data and asset lists
+def output_files(name, type):
+  makemydir("outputs/"+name)
+
+  newDir = "outputs/"+name
+
+  if type == "data":
+    makemydir(newDir+"/data")
+  elif type == "asset":
+    makemydir(newDir+"/assets")
+
+#outputting phase one
+makemydir("resource_packs")
+makemydir("behavior_packs")
+makemydir("outputs")
+
+#outputting phase two
+data_list = list_items("behavior_packs")
+asset_list = list_items("resource_packs")
+
+
+print(asset_list, data_list)
+
+#outputting phase three
+for item in data_list:
+  output_files(item, "data")
+
+for item in asset_list:
+  output_files(item, "asset")
