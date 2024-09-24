8b9511561ad84d766c4ffeab2d19fcc0125f2b4a
UltraHispeed-Elite <62296749+UltraHispeed-Elite@users.noreply.github.com>
2024-09-24 15:24:28 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os 
+import json
 
 #template code
 
@@ -28,8 +29,53 @@ def output_files(name, type):
 
   if type == "data":
     makemydir(newDir+"/data")
+    makemydir(newDir+"/data/fabricaddons")
   elif type == "asset":
     makemydir(newDir+"/assets")
+    makemydir(newDir+"/assets/fabricaddons")
+
+#phase four
+template = {
+  "parent": "minecraft:item/generated",
+  "textures": {
+    "layer0": 0
+  }
+}
+
+def modelConv(texture_source, output_id):
+    f = open(texture_source, "r")
+
+    test = f.read()
+
+    data = json.loads(test)
+
+    print(data)
+
+    textures = data['texture_data']
+
+    for item in textures:
+        print(item)
+        
+        model = template
+        
+        name = item.split(".")
+        new_name = name[1]
+        
+        print(name)
+        
+        item_id = textures[item]["textures"]
+        split_id = item_id.split("/")
+        print(split_id)
+        new_id = split_id[1]+"/"+split_id[2]
+        
+        f.close()
+
+        makemydir("outputs/"+output_id+"/assets/fabricaddons/model/item")
+        f = open("outputs/"+output_id+"/assets/fabricaddons/model/item/"+new_name+".json", "w")
+
+        model["textures"]["layer0"] = name[0]+":"+new_id
+        
+        f.write(json.dumps(model))
 
 #outputting phase one
 makemydir("resource_packs")
@@ -49,3 +95,6 @@ for item in data_list:
 
 for item in asset_list:
   output_files(item, "asset")
+
+for i in asset_list:
+  modelConv("resource_packs/"+i+"/textures/item_texture.json", i)
